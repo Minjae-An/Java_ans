c270c76aba8b2e757e1a88a1687f163f18dac131
calmmin <mj3242@naver.com>
2022-11-29 16:20:13 +0900

    problem 10 answer edited

--- chap13/src/Ans_10.java
+++ chap13/src/Ans_10.java
@@ -1,105 +1,108 @@
-import javax.swing.*;
 import java.awt.*;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
+import javax.swing.*;
+import java.awt.event.*;
+import static java.lang.Math.random;
 
-/*
-* 패널에 일정 딜레이로 랜덤하게 타원을 그리게하는 스레드 클래스
-* */
-class RandomOvalThread extends Thread{
-    private RandomOvalPanel rp;
-    /* repaint 를 호출할 패널 */
-    private final long DELAY=300;
-    /* 지연시간 */
+class MainFrame extends JFrame{
+    public MainFrame(){
+        setTitle("Make Drawing to Start");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-    public RandomOvalThread(RandomOvalPanel rp){
-        this.rp=rp;
+        setContentPane(new RandomCirclePanel());
+
+        setSize(400, 400);
+        setVisible(true);
     }
+}
 
-    /* 이 메서드를 호출할 경우 스레드에 wait 가 걸린다*/
-    synchronized public void pauseOperate(){
-        try{ this.wait(); }
-        catch(InterruptedException e){ return; }
-    }
+class RandomCirclePanel extends JPanel{
+    private int x, y, w, h;
+    private RandomCircle th=new RandomCircle();
+    public RandomCirclePanel(){
+        setLayout(null);
 
-    /* 이 메서드를 호출할 경우 스레드에 notify*/
-    synchronized public void startOperate(){
-        this.notify();
+        addComponentListener(new ComponentAdapter() {
+            @Override
+            public void componentResized(ComponentEvent e) {
+                JComponent jc=(JComponent)e.getSource();
+                x=(int)(random()*jc.getWidth()/2+100);
+                y=(int)(random()*jc.getHeight()/2+100);
+                w=(int)(random()*jc.getWidth()/4);
+                h=(int)(random()*jc.getHeight()/4);
+
+                removeComponentListener(this);
+            }
+        });
+
+        addMouseListener(new MouseAdapter(){
+            @Override
+            public void mouseEntered(MouseEvent e){
+                th.resumeOperation();
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e){
+                th.stopOperation();
+            }
+        });
+
+        th.start();
     }
 
     @Override
-    public void run(){
-        while(true){
-            try{
-                /*
-                wait 가 걸려있는 상태에서
-                패널에 마우스리스너를 부착하여 mouseEntered 이벤트가 발생할 경우
-                해당 마우스 리스너에서 startOperate 메서드를 호출하여 아래 코드를 실행
-                */
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
 
-                Thread.sleep(DELAY);
-                rp.repaint(); // 패널의 repaint 를 호출하여 랜덤한 위치에 타원 생성
+        g.setColor(new Color(139, 0, 255));
+        g.fillOval(x, y, w, h);
+    }
+
+    private class RandomCircle extends Thread{
+        private boolean isStop=false;
+
+        @Override
+        public void run(){
+            while(true){
+                try{
+                    Thread.sleep(300);
+                    if(isStop) stopRun();
+                    randomLocation();
+
+                    repaint();
+                }
+                catch(InterruptedException e){ return; }
             }
-            catch(InterruptedException e){ break; }
+        }
+
+        private void randomLocation(){
+            x=(int)(random()*getWidth()/2+100);
+            y=(int)(random()*getHeight()/2+100);
+            w=(int)(random()*getWidth()/4);
+            h=(int)(random()*getHeight()/4);
+        }
+
+        public void stopOperation(){
+            isStop=true;
+        }
+
+        synchronized private void stopRun(){
+            try{
+                this.wait();
+            }
+            catch(InterruptedException e){ return; }
+        }
+
+        synchronized public void resumeOperation(){
+            if(!isStop) return;
+
+            isStop=false;
+            this.notify();
         }
     }
 }
 
-/*
-랜덤하게 타원이 그려지는 패널
-*/
-class RandomOvalPanel extends JPanel {
-    private Color c=new Color(138,43,226);
-    public RandomOvalPanel(){
-        setLayout(null);
-    }
-
-    // 호출 시 패널내 랜덤한 위치에 타원이 그려진다.
-    @Override
-    public void paintComponent(Graphics g){
-        super.paintComponent(g);
-        g.setColor(c);
-        int x=(int)(Math.random()*getWidth());
-        int y=(int)(Math.random()*getHeight());
-        int w=(int)(Math.random()*getWidth()/2);
-        int h=(int)(Math.random()*getHeight()/2);
-
-        g.fillOval(x, y, w, h);
-    }
-}
-class MainFrame10 extends JFrame{
-    private RandomOvalPanel rp=new RandomOvalPanel();
-    private RandomOvalThread randomOvalThread=new RandomOvalThread(rp);
-    public MainFrame10(){
-        super("Make Drawing to Start");
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        setContentPane(rp);
-
-        rp.addMouseListener(new MouseAdapter(){
-            // 이 이벤트 발생시 스레드 notify
-            @Override
-            public void mouseEntered(MouseEvent e){
-                randomOvalThread.startOperate();
-            }
-
-            // 이 이벤트 발생시 스레드 wait
-            @Override
-            public void mouseExited(MouseEvent e){
-                randomOvalThread.pauseOperate();
-            }
-        });
-
-        setSize(400, 400);
-        setVisible(true);
-
-        // 스레드 작동 시작
-        randomOvalThread.start();
-    }
-}
-
-public class Ans_10 {
+public class Ans_10{
     public static void main(String[] args) {
-        new MainFrame10();
+        new MainFrame();
     }
 }
